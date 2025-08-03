1ccf84ffbd197a68f8317eeb3e48ade364e0225c
Keshav Dwivedi <keshavdwivedi2311@gmail.com>
2025-08-03 19:13:49 +0530

    Add Git Files and Pom

---
+++ src/test/java/com/sports/SmartSport/SmartSportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sports.SmartSport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
